a6e9761c864d58f45bc47c7b57fba447d19794a0
Pavel Bondar <pavlo.condar@crello.com>
2026-05-11 22:30:40 +0200

    project initialization

---
+++ KA_test/KA_test/KA_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  KA_testApp.swift
+//  KA_test
+//
+//  Created by Pavel Bondar on 11.05.26.
+//
+
+import SwiftUI
+
+@main
+struct KA_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KA_test/KA_testTests/KA_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  KA_testTests.swift
+//  KA_testTests
+//
+//  Created by Pavel Bondar on 11.05.26.
+//
+
+import Testing
+@testable import KA_test
+
+struct KA_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
